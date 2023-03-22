f76866b19997f68239471da82ada4a85ee755415
ReyolRhema <126499554+ReyolRhema@users.noreply.github.com>
2023-03-22 22:49:15 +0530

    Add files via upload

---
+++ computer.java
@@ -0,0 +1,8 @@
+package org.system;
+
+public class computer   {
+	public void computerModel () {
+	System.out.println("my computer model is DELL inspiron 15");
+
+}
+}
---
+++ desktop.java
@@ -0,0 +1,18 @@
+package org.system;
+
+public class desktop extends computer{
+	public void desktopSize() {
+		System.out.println("fifteen inches");}
+	public static void main(String[] args) {
+		
+	desktop Desktop = new desktop ();
+	 Desktop.computerModel();
+	 
+	 Desktop.desktopSize();
+	 
+			
+		
+		
+	}
+
+}
